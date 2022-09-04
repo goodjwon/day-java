064f22d500d90655fe71bae45447807d1ec8d121
goodjwon <firstman9@protonmail.com>
2022-09-04 21:12:07 +0900

    add 추가

--- src/demo/step1/Calculator.java
+++ src/demo/step1/Calculator.java
@@ -1,13 +1,22 @@
 package demo.step1;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("Please input a number");
-            return;
-        }
+        System.out.println("Please input a number");
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> list = new ArrayList<>();
 
-        System.out.println(args[0] + " + " + args[1] + " = " + (Integer.parseInt(args[0]) + Integer.parseInt(args[1])));
+        while (true){
+            list.add(Integer.parseInt(scanner.nextLine()));
+            if(list.size() == 2){
+                System.out.println(list.get(0) + " + " + list.get(1) + " = " + (add(list.get(0), list.get(1))));
+                break;
+            }
+        }
     }
 
     public static int add(int a, int b) {
