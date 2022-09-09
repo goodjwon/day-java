7824eeaf71ce1452ce896315f6b0812014f37136
goodjwon <firstman9@protonmail.com>
2022-09-09 12:20:19 +0900

    메소드 추가 버전 2

--- src/demo/step1/Calculator.java
+++ src/demo/step1/Calculator.java
@@ -13,7 +13,6 @@ public class Calculator {
         Integer result = 0;
 
         while (true){
-
             if(list.size() < 2){
                 list.add(Integer.parseInt(scanner.nextLine()));
             } else {
@@ -21,28 +20,44 @@ public class Calculator {
             }
 
             if(!command.equals("")){
-                switch (command){
-                    case "+" :
-                        result = add(list.get(0), list.get(1));
-                        break;
-                    case "-":
-                        result = minus(list.get(0), list.get(1));
-                        break;
-                    case "/":
-                        result = divide(list.get(0), list.get(1));
-                        break;
-                    case "*":
-                        result = multiply(list.get(0), list.get(1));
-                        break;
-                    default:
-                        System.out.println("지원하지 않는 연산 기호 입니다.");
-                        break;}
+                result = logical(list, command, result);
+                logicPrint(list, command, result);
 
-                System.out.println(list.get(0) + " + " + list.get(1) + " = ".concat(String.valueOf(result)));
-                list.clear();
-                command = "";
+                command = clear(list);
             }
         }
+    }
+
+    private static String clear(List<Integer> list) {
+        String command;
+        list.clear();
+        command = "";
+        return command;
+    }
+
+    private static void logicPrint(List<Integer> list, String command, Integer result){
+        System.out.println(list.get(0) +" "+ command +" "+ list.get(1) + " = ".concat(String.valueOf(result)));
+    }
+
+    private static Integer logical(List<Integer> list, String command, Integer result) {
+        switch (command){
+            case "+" :
+                result = add(list.get(0), list.get(1));
+                break;
+            case "-":
+                result = minus(list.get(0), list.get(1));
+                break;
+            case "/":
+                result = divide(list.get(0), list.get(1));
+                break;
+            case "*":
+                result = multiply(list.get(0), list.get(1));
+                break;
+            default:
+                System.out.println("지원하지 않는 연산 기호 입니다.");
+                break;}
+
+        return result;
     }
 
     public static int add(int a, int b) {
